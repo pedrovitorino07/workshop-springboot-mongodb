c22f102467b72407e6bf263ad2c01b2c408309f6
Pedro Vitorino <pedrolucasofici@gmail.com>
2025-07-04 17:24:22 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/pedrovitorino/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.pedrovitorino.workshopmongo.services;
+
+import com.pedrovitorino.workshopmongo.domain.User;
+import com.pedrovitorino.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
