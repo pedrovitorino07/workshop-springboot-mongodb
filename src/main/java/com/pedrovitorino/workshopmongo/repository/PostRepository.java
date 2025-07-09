743837daf82922996a1d2794ce5bada83a0f6a4f
Pedro Vitorino <pedrolucasofici@gmail.com>
2025-07-09 15:43:46 -0300

    Criando entity Post com user alinhado

---
+++ src/main/java/com/pedrovitorino/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.pedrovitorino.workshopmongo.repository;
+
+import com.pedrovitorino.workshopmongo.domain.Post;
+import com.pedrovitorino.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
