4919cce18c80ba67e2114b4bbb0171b6f67b464c
Pedro Vitorino <pedrolucasofici@gmail.com>
2025-07-09 15:58:35 -0300

    Referenciando os posts do usuario

--- src/main/java/com/pedrovitorino/workshopmongo/config/Instantiation.java
+++ src/main/java/com/pedrovitorino/workshopmongo/config/Instantiation.java
@@ -13,6 +13,8 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.TimeZone;
 
+import static java.util.Collections.addAll;
+
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
@@ -41,5 +43,8 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
